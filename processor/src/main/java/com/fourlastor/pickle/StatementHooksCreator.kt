d66fce815ed9bc89e47af51f97803c77087a2317
Said Tahsin Dane <tasomaniac@gmail.com>
2019-03-03 13:37:33 +0100

    Order hook methods by Cucumber order

--- processor/src/main/java/com/fourlastor/pickle/StatementHooksCreator.kt
+++ processor/src/main/java/com/fourlastor/pickle/StatementHooksCreator.kt
@@ -15,17 +15,28 @@ class StatementHooksCreator(private val roundEnv: RoundEnvironment) {
             generateStatementsFor(After::class.java, roundEnv)
 
     private fun generateStatementsFor(
-            annotation: Class<out Annotation>,
+            annotationType: Class<out Annotation>,
             roundEnv: RoundEnvironment
     ): List<TestMethodStatement> {
-        val methods = roundEnv.getMethodsAnnotatedWith(annotation)
+        val methods = roundEnv.getMethodsAnnotatedWith(annotationType)
 
-        return methods.map { method ->
+        return methods.sortedBy {
+            it.annotationOrder(annotationType)
+        }.map { method ->
             val field = TestField(method.enclosingElement as TypeElement)
             testMethodStatement(field, "\$N.\$N()", method.simpleName)
         }
     }
+
+    private fun ExecutableElement.annotationOrder(annotationType: Class<out Annotation>) =
+            when (val annotation = getAnnotation(annotationType)) {
+                is Before -> annotation.order
+                is After -> annotation.order
+                else -> DEFAULT_CUCUMBER_HOOK_ORDER
+            }
 }
+
+private const val DEFAULT_CUCUMBER_HOOK_ORDER = 10000
 
 private fun <T : Annotation> RoundEnvironment.getMethodsAnnotatedWith(clazz: Class<T>): List<ExecutableElement> {
     return getElementsAnnotatedWith(clazz)

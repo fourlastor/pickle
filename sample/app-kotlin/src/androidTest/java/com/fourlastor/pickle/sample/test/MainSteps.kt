7673955b8049c393deeb59dbc8e7343be31025ef
Said Tahsin Dane <tasomaniac@gmail.com>
2019-08-02 21:16:02 +0200

    Add sample with Kotlin

---
+++ sample/app-kotlin/src/androidTest/java/com/fourlastor/pickle/sample/test/MainSteps.kt
@@ -0,0 +1,16 @@
+package com.fourlastor.pickle.sample.test
+
+import cucumber.api.java.en.Then
+import cucumber.api.java.en.When
+
+class MainSteps {
+
+    @When("^user launches the app$")
+    fun userLaunchesTheApp() {
+
+    }
+
+    @Then("^they should see welcome message$")
+    fun theyShouldSeeWelcomeMessage() {
+    }
+}
